a57ca1506ea332bea773c632e7c7ec0213bf3341
unknown <arthurlps123@gmail.com>
2022-08-06 06:10:39 -0300

    formulário simples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Form from "./Form.js"
+import "./App.css"
 
-function App() {
+export default function App() {
+
+  const formName = "Formulário";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Form formName={formName}  />
+  )
 
-export default App;
+}
---
+++ src/Form.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function Form({ formName }) {
+
+  const [formField, setFormField] = useState({});
+
+  const handleValue = (e) => {
+    const attField = e.target.name;
+    const newValue = e.target.value;
+
+    setFormField({
+      [attField]: newValue
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+  }
+
+  console.log(formField)
+
+  return (
+    <div>
+      <div className="center">
+        <h1>{formName}</h1>
+        <form onSubmit={handleSubmit}>
+
+          <div className="inputbox">
+            <input type="text" name="name" required="required" value={formField.name || ""} onChange={handleValue} />
+            <span>Email</span>
+          </div>
+
+          <div className="inputbox">
+            <input type="password" name="password" required="required" value={formField.password || ""} onChange={handleValue} />
+            <span>Password</span>
+          </div>
+        </form>
+
+        <div id="button">
+          <button className="button" type="submit">Submit</button></div>
+        </div>
+
+      </div>
+      
+
+  );
+}
